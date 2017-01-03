2ddb7865003c55d8cddabec2650a65c7cd777b1e
duan lin <linduan@cndoppler.cn>
2017-01-03 11:14:38 +0800

    添加dma代码，代码在linux3.0.35内核编译测试通过可行

--- dma_sg.c
+++ dma_sg.c
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2006-2012 Freescale Semiconductor, Inc. All rights reserved.
+ */
+
+/*
+ * The code contained herein is licensed under the GNU General Public
+ * License. You may obtain a copy of the GNU General Public License
+ * Version 2 or later at the following locations:
+ *
+ * http://www.opensource.org/licenses/gpl-license.html
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/sched.h>
@@ -7,27 +20,41 @@
 #include <linux/fs.h>
 #include <linux/version.h>
 #include <linux/delay.h>
+#include <mach/dma.h>
 
 #include <linux/dmaengine.h>
 #include <linux/device.h>
-#include <linux/platform_data/dma-imx.h>
-#include <linux/platform_data/dma-imx-sdma.h>
 
 #include <linux/io.h>
 #include <linux/delay.h>
+//#include <sys/time.h>
+
+#define DEBUG
+#define DEF_SG_QTY 3
+#ifdef DEBUG
+/* If you are writing a driver, please use dev_dbg instead */
+	#define dev_printk(fmt, args...)  printk(KERN_DEBUG fmt, ## args)
+#else
+	static /*inline*/ int __attribute__ ((format (printf, 1, 2))) dev_printk(const char * fmt, ...)
+	{
+		return 0;
+	}
+#endif
 
 static int gMajor; /* major number of device */
 static struct class *dma_tm_class;
-u32 *wbuf, *wbuf1, *wbuf2, *wbuf3, *wbuf4;
-u32 *rbuf, *rbuf1, *rbuf2, *rbuf3, *rbuf4;
+u32 *wbuf[DEF_SG_QTY];
+u32 *rbuf[DEF_SG_QTY];
 
 struct dma_chan *dma_m2m_chan;
 
 struct completion dma_m2m_ok;
 
-struct scatterlist sg[4], sg2[4];
+struct scatterlist sg[DEF_SG_QTY], sg2[DEF_SG_QTY];
 
-#define SDMA_BUF_SIZE  1024
+#define SDMA_BUF_SIZE  40960
+
+
 
 static bool dma_m2m_filter(struct dma_chan *chan, void *param)
 {
@@ -37,10 +64,11 @@ static bool dma_m2m_filter(struct dma_chan *chan, void *param)
 	return true;
 }
 
-int sdma_open(struct inode *inode, struct file *filp)
+int sdma_open(struct inode * inode, struct file * filp)
 {
+	int i;
 	dma_cap_mask_t dma_m2m_mask;
-	struct imx_dma_data m2m_dma_data;
+	struct imx_dma_data m2m_dma_data = {0};
 
 	init_completion(&dma_m2m_ok);
 
@@ -48,265 +76,166 @@ int sdma_open(struct inode *inode, struct file *filp)
 	dma_cap_set(DMA_SLAVE, dma_m2m_mask);
 	m2m_dma_data.peripheral_type = IMX_DMATYPE_MEMORY;
 	m2m_dma_data.priority = DMA_PRIO_HIGH;
-	dma_m2m_chan = dma_request_channel(dma_m2m_mask, dma_m2m_filter,
-								&m2m_dma_data);
+
+	dma_m2m_chan = dma_request_channel(dma_m2m_mask, dma_m2m_filter, &m2m_dma_data);
 	if (!dma_m2m_chan) {
-		printk("Error opening the SDMA memory to memory channel\n");
+		dev_printk("Error opening the SDMA memory to memory channel\n");
 		return -EINVAL;
 	}
-	printk ("%s %d \n", __func__, __LINE__);
 
-	wbuf = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!wbuf) {
-		printk("error wbuf !!!!!!!!!!!\n");
-		return -1;
+	for(i=0; i<DEF_SG_QTY; i++){
+		wbuf[i] = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
+		if(!wbuf[i]) {
+			dev_printk("error wbuf[%d] !!!!!!!!!!!\n", i);
+			return -1;
+		}
 	}
-	printk ("%s %d \n", __func__, __LINE__);
 
-	wbuf1 = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!wbuf1) {
-		printk("error wbuf1 !!!!!!!!!!!\n");
-		return -1;
+	dev_printk("kzalloc %d wbuf succeed\n", DEF_SG_QTY);
+
+
+
+
+
+
+	for(i=0; i<DEF_SG_QTY; i++){
+		rbuf[i] = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
+		if(!rbuf[i]) {
+			dev_printk("error rbuf[%d] !!!!!!!!!!!\n", i);
+			return -1;
+		}
 	}
-	printk ("%s %d \n", __func__, __LINE__);
-
-	wbuf2 = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!wbuf2) {
-		printk("error wbuf2 !!!!!!!!!!!\n");
-		return -1;
-	}
-	printk ("%s %d \n", __func__, __LINE__);
-
-	wbuf3 = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!wbuf3) {
-		printk("error wbuf3 !!!!!!!!!!!\n");
-		return -1;
-	}
-	printk ("%s %d \n", __func__, __LINE__);
-
-	wbuf4 = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!wbuf4) {
-		printk("error wbuf4 !!!!!!!!!!!\n");
-		return -1;
-	}
-	printk ("%s %d \n", __func__, __LINE__);
-
-	rbuf = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!rbuf) {
-		printk("error rbuf !!!!!!!!!!!\n");
-		return -1;
-	}
-	printk ("%s %d \n", __func__, __LINE__);
-
-	rbuf1 = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!rbuf1) {
-		printk("error rbuf1 !!!!!!!!!!!\n");
-		return -1;
-	}
-	printk ("%s %d \n", __func__, __LINE__);
-
-	rbuf2 = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!rbuf2) {
-		printk("error rbuf2 !!!!!!!!!!!\n");
-		return -1;
-	}
-	printk ("%s %d \n", __func__, __LINE__);
-
-	rbuf3 = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!rbuf3) {
-		printk("error rbuf3 !!!!!!!!!!!\n");
-		return -1;
-	}
-	printk ("%s %d \n", __func__, __LINE__);
-
-	rbuf4 = kzalloc(SDMA_BUF_SIZE, GFP_DMA);
-	if(!rbuf4) {
-		printk("error rbuf4 !!!!!!!!!!!\n");
-		return -1;
-	}
-	printk ("%s %d \n", __func__, __LINE__);
-
+	dev_printk("kzalloc %d rbuf succeed\n", DEF_SG_QTY);
 	return 0;
 }
 
 int sdma_release(struct inode * inode, struct file * filp)
 {
-	dmaengine_terminate_all(dma_m2m_chan);
+	int i;
 	dma_release_channel(dma_m2m_chan);
 	dma_m2m_chan = NULL;
-	kfree(wbuf);
-	kfree(rbuf);
+
+	for(i=0; i<DEF_SG_QTY; i++)
+	{
+		kfree(wbuf[i]);
+		kfree(rbuf[i]);
+	}
+
 	return 0;
 }
 
 ssize_t sdma_read (struct file *filp, char __user * buf, size_t count,
-loff_t * offset)
+								loff_t * offset)
 {
-	int i;
-/*	for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		if (*(rbuf+i) != *(wbuf+i)) {
-			printk("buffer 0 copy falled!\n");
-			return 0;
-		}
-	}
-	printk("buffer 0 copy passed!\n");
+	int i,j;
 
 	for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		if (*(rbuf1+i) != *(wbuf1+i)) {
-			printk("buffer 1 copy falled!\n");
-			return 0;
+		for(j=0; j<DEF_SG_QTY; j++){
+			if(*(rbuf[j]+i) != *(wbuf[j]+i)){
+				dev_printk("buf[%d]: %x -- %x\n", i, *(rbuf[j]+i), *(wbuf[j]+i));
+			}
 		}
+		
 	}
-	printk("buffer 1 copy passed!\n");
 
-	for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		if (*(rbuf2+i) != *(wbuf2+i)) {
-			printk("buffer 2 copy falled!\n");
-			return 0;
-		}
-	}
-	printk("buffer 2 copy passed!\n");
-
-	for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		if (*(rbuf3+i) != *(wbuf3+i)) {
-			printk("buffer 3 copy falled!\n");
-			return 0;
-		}
-	}
-	printk("buffer 3 copy passed!\n");
-*/
-	for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		if (*(rbuf4+i) != *(wbuf4+i)) {
-			printk("buffer 4 copy falled!\n");
-			return 0;
-		}
-	}
-	printk("buffer 4 copy passed!\n");
+	dev_printk("buffer 1 copy passed!\n");
 
 	return 0;
 }
 
-
 static void dma_m2m_callback(void *data)
 {
-	printk ("%s %s %d \n", __FILE__, __func__, __LINE__);
+	dev_printk("  *** [%s]\n",__func__);
 	complete(&dma_m2m_ok);
 	return ;
 }
 
 ssize_t sdma_write(struct file * filp, const char __user * buf, size_t count,
-        loff_t * offset)
+								loff_t * offset)
 {
-	 u32 *index1, *index2, *index3, i, ret;
-	 struct dma_slave_config dma_m2m_config = {0};
-	 struct dma_async_tx_descriptor *dma_m2m_desc;
-	 u32 *index4 = wbuf4;
-	 dma_addr_t dma_src, dma_dst;
+	u32 *index[3], i, ret, cnt;
+	long start, end;
+	struct dma_slave_config dma_m2m_config;
+	struct dma_async_tx_descriptor *dma_m2m_desc;
+	struct timeval end_time;
 
-	 index1 = wbuf;
-	 index2 = wbuf2;
-	 index3 = wbuf3;
+	for(cnt=0; cnt<DEF_SG_QTY; cnt++){
+		index[cnt] = wbuf[cnt];
+	}
 
-	 for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		  *(index1 + i) = 0x12121212;
-	 }
-	printk ("%s %d \n", __func__, __LINE__);
 
-	 for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		  *(index2 + i) = 0x34343434;
-	 }
-	printk ("%s %d \n", __func__, __LINE__);
+	for (i=0; i<SDMA_BUF_SIZE/4; i++) {
+		*(index[0] + i) = 0x12121212;
+	}
 
-	 for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		  *(index3 + i) = 0x56565656;
-	 }
-	printk ("%s %d \n", __func__, __LINE__);
+	for (i=0; i<SDMA_BUF_SIZE/4; i++) {
+		*(index[1] + i) = 0x34343434;
+	}
 
-	 for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		  *(index4 + i) = 0x78787878;
-	 }
-	printk ("%s %d \n", __func__, __LINE__);
+	for (i=0; i<SDMA_BUF_SIZE/4; i++) {
+		*(index[2] + i) = 0x56565656;
+	}
 
-#if 0
-	 for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		 printk("input data_%d : %x\n", i, *(wbuf+i));
-	 }
 
-	 for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		 printk("input data2_%d : %x\n", i, *(wbuf2+i));
-	 }
+	dma_m2m_config.direction = DMA_MEM_TO_MEM;
+	dma_m2m_config.dst_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES;
+	dmaengine_slave_config(dma_m2m_chan, &dma_m2m_config);
 
-	 for (i=0; i<SDMA_BUF_SIZE/4; i++) {
-		 printk("input data3_%d : %x\n", i, *(wbuf3+i));
-	 }
-#endif
-	 dma_m2m_config.direction = DMA_MEM_TO_MEM;
-	 dma_m2m_config.dst_addr_width = DMA_SLAVE_BUSWIDTH_2_BYTES;
-	 dmaengine_slave_config(dma_m2m_chan, &dma_m2m_config);
+	dev_printk(KERN_ERR "%s:%d\n", __func__, __LINE__);
+	sg_init_table(sg, DEF_SG_QTY);
+	for(cnt=0; cnt<DEF_SG_QTY; cnt++){
+		sg_set_buf(&sg[cnt], wbuf[cnt], SDMA_BUF_SIZE);
+	}
+	ret = dma_map_sg(NULL, sg, DEF_SG_QTY, dma_m2m_config.direction);
 
-/*	 sg_init_table(sg, 3);
-	 sg_set_buf(&sg[0], wbuf, SDMA_BUF_SIZE);
-	 sg_set_buf(&sg[1], wbuf2, SDMA_BUF_SIZE/2);
-	 sg_set_buf(&sg[2], wbuf3, SDMA_BUF_SIZE);
-	 ret = dma_map_sg(NULL, sg, 3, dma_m2m_config.direction);
+	dev_printk(KERN_ERR "%s:%d\n", __func__, __LINE__);
+	dma_m2m_desc = dma_m2m_chan->device->device_prep_slave_sg(dma_m2m_chan,sg, DEF_SG_QTY, dma_m2m_config.direction, 1);
+	dev_printk(KERN_ERR "%s:%d first m2m_desc=%p\n", __func__, __LINE__, dma_m2m_desc);
 
-#if (LINUX_VERSION_CODE <= KERNEL_VERSION(3,0,35))
-	 dma_m2m_desc = dma_m2m_chan->device->device_prep_slave_sg(dma_m2m_chan,sg, 3, dma_m2m_config.direction, 1);
-#endif
+	dev_printk(KERN_ERR "%s:%d\n", __func__, __LINE__);
 
-	 sg_init_table(sg2, 3);
-	 sg_set_buf(&sg2[0], rbuf, SDMA_BUF_SIZE);
-	 sg_set_buf(&sg2[1], rbuf2, SDMA_BUF_SIZE/2);
-	 sg_set_buf(&sg2[2], rbuf3, SDMA_BUF_SIZE);
-	 ret = dma_map_sg(NULL, sg2, 3, dma_m2m_config.direction);
+	sg_init_table(sg2, DEF_SG_QTY);
+	for(cnt=0; cnt<DEF_SG_QTY; cnt++){
+		sg_set_buf(&sg2[cnt], rbuf[cnt], SDMA_BUF_SIZE);
+	}
 
-#if (LINUX_VERSION_CODE <= KERNEL_VERSION(3,0,35))
-	printk ("dma_m2m_chan->device->device_prep_slave_sg = 0x%x\n", (u32)dma_m2m_chan->device->device_prep_slave_sg);
-	 dma_m2m_desc = dma_m2m_chan->device->device_prep_slave_sg(dma_m2m_chan,sg2, 3, dma_m2m_config.direction, 0);
-	printk ("dma_m2m_desc = 0x%x \n", (u32)dma_m2m_desc);
-#else
-	printk (" dma_m2m_chan->device->device_prep_dma_sg = 0x%x\n", (u32)dma_m2m_chan->device->device_prep_dma_sg);
-	dma_m2m_desc = dma_m2m_chan->device->device_prep_dma_sg(dma_m2m_chan,sg2, 3, sg, 3, 0);
-	printk ("dma_m2m_desc = 0x%x \n", (u32)dma_m2m_desc);
-#endif
+	ret = dma_map_sg(NULL, sg2, DEF_SG_QTY, dma_m2m_config.direction);
+	dev_printk(KERN_ERR "%s:%d ret=%d\n", __func__, __LINE__, ret);
 
-	 dma_m2m_desc->callback = dma_m2m_callback;
-	 dmaengine_submit(dma_m2m_desc);
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,35))
-	 dma_async_issue_pending(dma_m2m_chan);
-#endif
+	dma_m2m_desc = dma_m2m_chan->device->device_prep_slave_sg(dma_m2m_chan,sg2, DEF_SG_QTY, dma_m2m_config.direction, 0);
+	dev_printk(KERN_ERR "%s:%d m2m_desc=%p\n", __func__, __LINE__, dma_m2m_desc);
 
-	 wait_for_completion(&dma_m2m_ok);
-	 dma_unmap_sg(NULL, sg, 3, dma_m2m_config.direction);
-	 dma_unmap_sg(NULL, sg2, 3, dma_m2m_config.direction);
-*/
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,35))
-	 dma_src = dma_map_single(NULL, wbuf4, SDMA_BUF_SIZE, DMA_TO_DEVICE);
-	 dma_dst = dma_map_single(NULL, rbuf4, SDMA_BUF_SIZE, DMA_FROM_DEVICE);
-	printk ("dma_m2m_chan->device->device_prep_dma_memcpy = 0x%x\n", (u32)dma_m2m_chan->device->device_prep_dma_memcpy);
-	 dma_m2m_desc = dma_m2m_chan->device->device_prep_dma_memcpy(dma_m2m_chan, dma_dst, dma_src, SDMA_BUF_SIZE,0);
-	printk ("dma_m2m_desc = 0x%x \n", (u32)dma_m2m_desc);
-	 if (!dma_m2m_desc) {
-		printk("prep error!!\n");
-		return -1;
- 	 }
-	 dma_m2m_desc->callback = dma_m2m_callback;
-	 dmaengine_submit(dma_m2m_desc);
-	 dma_async_issue_pending(dma_m2m_chan);
-	 wait_for_completion(&dma_m2m_ok);
-	 dma_unmap_single(NULL, dma_src, SDMA_BUF_SIZE, DMA_TO_DEVICE);
-	 dma_unmap_single(NULL, dma_dst, SDMA_BUF_SIZE, DMA_FROM_DEVICE);
-#endif
+	dev_printk(KERN_ERR "%s:%d device_prep_dma_memcpy=%p\n", __func__, __LINE__, dma_m2m_chan->device->device_prep_dma_memcpy);
+	dev_printk(KERN_ERR "%s:%d device_prep_dma_sg=%p\n", __func__, __LINE__, dma_m2m_chan->device->device_prep_dma_sg);
+	
+	do_gettimeofday(&end_time);
+	start = end_time.tv_sec*1000000 + end_time.tv_usec;
 
-	 return 0;
+
+	dma_m2m_desc->callback = dma_m2m_callback;
+	dev_printk(KERN_ERR "%s:%d submitting for dma\n", __func__, __LINE__);
+	dmaengine_submit(dma_m2m_desc);
+
+	dev_printk(KERN_ERR "%s:%d waiting for completion\n", __func__, __LINE__);
+	wait_for_completion(&dma_m2m_ok);
+
+	do_gettimeofday(&end_time);
+	end = end_time.tv_sec*1000000 + end_time.tv_usec;
+	printk("end - start = %ld us\n", end - start);
+
+	dma_unmap_sg(NULL, sg, DEF_SG_QTY, dma_m2m_config.direction);
+	dma_unmap_sg(NULL, sg2, DEF_SG_QTY, dma_m2m_config.direction);
+
+
+	dev_printk(KERN_ERR "%s:%d count=%d\n", __func__, __LINE__, count);
+	return count;
 }
 
-
 struct file_operations dma_fops = {
-	open: sdma_open,
-	release: sdma_release,
-	read: sdma_read,
-	write: sdma_write,
+	open:		sdma_open,
+	release:	sdma_release,
+	read:		sdma_read,
+	write:		sdma_write,
 };
 
 int __init sdma_init_module(void)
@@ -318,41 +247,41 @@ int __init sdma_init_module(void)
 #endif
 	int error;
 
-/* register a character device */
+	/* register a character device */
 	error = register_chrdev(0, "sdma_test", &dma_fops);
 	if (error < 0) {
-		printk("SDMA test driver can't get major number\n");
+		dev_printk("SDMA test driver can't get major number\n");
 		return error;
 	}
 	gMajor = error;
-	printk("SDMA test major number = %d\n",gMajor);
+	dev_printk("SDMA test major number = %d\n",gMajor);
 
 	dma_tm_class = class_create(THIS_MODULE, "sdma_test");
 	if (IS_ERR(dma_tm_class)) {
-		printk(KERN_ERR "Error creating sdma test module class.\n");
+		dev_printk(KERN_ERR "Error creating sdma test module class.\n");
 		unregister_chrdev(gMajor, "sdma_test");
 		return PTR_ERR(dma_tm_class);
 	}
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,28))
 	temp_class = device_create(dma_tm_class, NULL,
-		  MKDEV(gMajor, 0), NULL, "sdma_test");
+				   MKDEV(gMajor, 0), NULL, "sdma_test");
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,26))
 	temp_class = device_create(dma_tm_class, NULL,
-		  MKDEV(gMajor, 0), "sdma_test");
+				   MKDEV(gMajor, 0), "sdma_test");
 #else
 	temp_class = class_device_create(dma_tm_class, NULL,
-		    MKDEV(gMajor, 0), NULL,
-		    "sdma_test");
+					     MKDEV(gMajor, 0), NULL,
+					     "sdma_test");
 #endif
 	if (IS_ERR(temp_class)) {
-		printk(KERN_ERR "Error creating sdma test class device.\n");
+		dev_printk(KERN_ERR "Error creating sdma test class device.\n");
 		class_destroy(dma_tm_class);
 		unregister_chrdev(gMajor, "sdma_test");
 		return -1;
 	}
 
-	printk("SDMA test Driver Module loaded\n");
+	dev_printk("SDMA test Driver Module loaded\n");
 	return 0;
 }
 
@@ -366,9 +295,13 @@ static void sdma_cleanup_module(void)
 #endif
 	class_destroy(dma_tm_class);
 
-	printk("SDMA test Driver Module Unloaded\n");
+	dev_printk("SDMA test Driver Module Unloaded\n");
 }
+
 
 module_init(sdma_init_module);
 module_exit(sdma_cleanup_module);
+
+MODULE_AUTHOR("Freescale Semiconductor");
+MODULE_DESCRIPTION("SDMA test driver");
 MODULE_LICENSE("GPL");
