3b0bea36c2c33fe86de76c733302436e6c42b6b4
王 胜 <wangsheng@cndoppler.cn>
2017-01-20 13:09:34 +0800

    修改README

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # obj-m +=mxc_sdma_memcopy_test.o
 obj-m +=mem_2_mem_demo.o
 all:
-	make -C /home/ws/kernel/linux SUBDIRS=$(PWD) modules
+	make -C /home/ws/kernel/linux-4.x SUBDIRS=$(PWD) modules
 clean:
-	make -C /home/ws/kernel/linux SUBDIRS=$(PWD) clean 
+	make -C /home/ws/kernel/linux-4.x SUBDIRS=$(PWD) clean 
--- copy.sh
+++ copy.sh
@@ -6,11 +6,6 @@
 #	echo "the dma's driver is not builded."
 #	exit
 # else
-#	scp dma_drv.ko root@192.168.1.215:/tmp
-#	scp dma_dev.ko root@192.168.1.215:/tmp
-#	scp insmod.sh  root@192.168.1.215:/tmp
- 	scp mem_2_mem_demo.ko root@192.168.1.224:/tmp
+ 	scp mxc_sdma_memcopy_test.ko root@192.168.1.224:/tmp
 	scp mxc_test root@192.168.1.224:/tmp
-#	scp dma_sg.ko root@192.168.1.224:/tmp
-#	scp dma.ko root@192.168.1.224:/tmp
 # fi
