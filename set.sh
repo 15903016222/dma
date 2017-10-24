f6aaf8a59b1e36380e451e47ceca7a6c6d81f285
wangsheng <wangsheng@cndoppler.cn>
2017-10-24 11:34:45 +0800

    add: DMA寄存器的配置

--- set.sh
+++ set.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# insmod eim_register_set.ko
 insmod one_register_modu.ko
 
+# config EIM PAD mux funtion
 ./one_register write 0x20e0114 0x0
 ./one_register write 0x20e0118 0x0
 ./one_register write 0x20e013c 0x0
@@ -11,3 +11,11 @@ insmod one_register_modu.ko
 ./one_register write 0x20e00f8 0x0
 ./one_register write 0x20e010c 0x0
 ./one_register write 0x20e0110 0x0
+
+# confi EIM register
+./one_register write 0x21b8000 0x00010089
+./one_register write 0x21b8008 0x0a020000
+
+insmod DmaDoppler-II.ko
+
+sleep 1
