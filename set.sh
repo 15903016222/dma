661c1106c41324d6f1c3b407c7edbffe36b64345
wangsheng <wangsheng@cndoppler.cn>
2018-01-13 10:08:34 +0800

    修改：EIM_EB0 EIM_EB1的EIM复用功能，这两个使用的是gpio功能，用于冻结与编码器复位

--- set.sh
+++ set.sh
@@ -9,8 +9,6 @@ insmod one_register_modu.ko
 ./one_register write 0x20e0140 0x0
 ./one_register write 0x20e0144 0x0
 ./one_register write 0x20e00f8 0x0
-./one_register write 0x20e010c 0x0
-./one_register write 0x20e0110 0x0
 
 # confi EIM register
 ./one_register write 0x21b8000 0x00010089
